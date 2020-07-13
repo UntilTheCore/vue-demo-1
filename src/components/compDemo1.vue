4864adca2ea2d2f2458383baeda3fb9769382a09
UntilTheCore <814898862@qq.com>
2020-07-13 12:25:07 +0800

    熟悉、使用Vue组件，单文件组件和全局组件。

---
+++ src/components/compDemo1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>我是组件1</div>
+</template>
+
+<script>
+    export default {
+        name: "compDemo1"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,29 +1,48 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 // import App from './App.vue'
-//
+
+// 使用cdn引入的Vue
+const Vue = window.Vue
 Vue.config.productionTip = false
 
 // new Vue({
 //   render: h => h(App),
 // }).$mount('#app')
-console.log(window.Vue)
+// console.log(window.Vue)
 
+import CompDemo1 from './components/compDemo1'
 // 1、完整版的Vue操作html
-// new Vue({
-//     el:'#app',
-//     // template:`
-//     //     <div>helloworld!</div>
-//     // `,
-//     data:{
-//         n:1,
-//     },
-//     methods:{
-//         add(){
-//             this.n += 1
-//         }
-//
-//     }
-// })
+//  1.1 - 使用单文件组件
+//  1.2 - 创建和使用全局组件
+Vue.component('CompDemo2', {
+    template: `
+        <div>
+        我是组件2
+</div>
+    `
+})
+new Vue({
+    components: {
+        CompDemo1,
+    },
+    el: '#app',
+    template: `
+        <div>
+            <CompDemo1/>
+            <comp-demo2/>
+        </div>
+
+    `,
+    data: {
+        n: 1,
+    },
+    methods: {
+        add() {
+            this.n += 1
+        }
+
+    }
+})
 
 // 2、非完整版的Vue需要通过 h 去渲染
 // 它是通过 render 方法内渲染出的内容去替换掉 #app，包括 app 本身
@@ -41,16 +60,16 @@ console.log(window.Vue)
 // })
 
 // 3、使用vue单文件组件渲染
-import app1 from './App1'
+// import app1 from './App1'
 // 通过 vue-loader 会将 后缀为vue的文件加载为一个对象供render去渲染。
-console.log(app1)
-console.log(app1.render)
-new Vue({
-    el:'#app',
-    render(h) {
-        console.log(h)
-        return h(app1)
-    }
-})
+// console.log(app1)
+// console.log(app1.render)
+// new Vue({
+//     el:'#app',
+//     render(h) {
+//         console.log(h)
+//         return h(app1)
+//     }
+// })
 
 
